e053ebce6e150ffd26e1d5674ee8ff391604c117
ed-vieira <edfelip@gmail.com>
2021-09-20 17:05:40 -0300

    sequelize migration

---
+++ migrations/20210918045413-create-students.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      cpf: {
+        type: Sequelize.STRING(20)
+      },
+      name: {
+        type: Sequelize.STRING(120)
+      },
+      email: {
+        type: Sequelize.STRING(120)
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('students');
+  }
+};
